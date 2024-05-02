18ba2a8c65591447e6bac200ecba80c7cd7fa5ce
Aina Tiavina <rjls.tiavina@gmail.com>
2024-05-02 18:18:03 +0300

    add:components

---
+++ lib/shared/components/shape/blurred_circle_shape.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import '../../../core/utils/colors/app_color.dart';
+
+class BlurredCircleShape extends StatelessWidget {
+  const BlurredCircleShape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageFiltered(
+      imageFilter: ImageFilter.blur(
+        sigmaX: 130,
+        sigmaY: 130,
+        tileMode: TileMode.decal,
+      ),
+      child: CircleAvatar(
+        backgroundColor: AppColor.yellow1.withOpacity(0.8),
+        radius: 150,
+      ),
+    );
+  }
+}
