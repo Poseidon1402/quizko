81dc3763208f6c46351e44dfe7b604c263b6b484
Aina Tiavina <rjls.tiavina@gmail.com>
2024-05-15 20:55:58 +0300

    done:bottom navigation bar

---
+++ lib/core/utils/constants/main_tab.dart
@@ -0,0 +1,8 @@
+import 'dart:core';
+
+abstract class MainTab {
+  static const int home = 0;
+  static const int result = 1;
+  static const int setting = 2;
+  static const int account = 3;
+}
--- lib/features/main/presentation/screens/main_screen.dart
+++ lib/features/main/presentation/screens/main_screen.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/utils/colors/app_color.dart';
+import '../../../../core/utils/constants/main_tab.dart';
 import '../components/bottom_app_bar_item.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
 
   const MainScreen({
@@ -13,9 +14,22 @@ class MainScreen extends StatelessWidget {
   });
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late int currentIndex;
+
+  @override
+  void initState() {
+    currentIndex = widget.navigationShell.currentIndex;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: navigationShell,
+      body: widget.navigationShell,
       extendBody: true,
       bottomNavigationBar: BottomAppBar(
         padding: EdgeInsets.zero,
@@ -26,23 +40,27 @@ class MainScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             BottomAppBarItem(
-              onTap: () {},
+              onTap: () => setState(() => currentIndex = MainTab.home),
               icon: 'home',
+              isSelected: currentIndex == MainTab.home,
               label: 'Home',
             ),
             BottomAppBarItem(
-              onTap: () {},
+              onTap: () => setState(() => currentIndex = MainTab.result),
               icon: 'results',
+              isSelected: currentIndex == MainTab.result,
               label: 'Result',
             ),
             BottomAppBarItem(
-              onTap: () {},
+              onTap: () => setState(() => currentIndex = MainTab.setting),
               icon: 'setting',
+              isSelected: currentIndex == MainTab.setting,
               label: 'Settings',
             ),
             BottomAppBarItem(
-              onTap: () {},
+              onTap: () => setState(() => currentIndex = MainTab.account),
               icon: 'user',
+              isSelected: currentIndex == MainTab.account,
               label: 'Account',
             ),
           ],
