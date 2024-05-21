5657a2a35ef80392717c4e78e619072e4c10b12c
Aina Tiavina <rjls.tiavina@gmail.com>
2024-05-21 11:44:51 +0300

    done:setting screen

--- lib/features/settings/presentation/screens/setting_screen.dart
+++ lib/features/settings/presentation/screens/setting_screen.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue,);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(color: Theme.of(context).colorScheme.onSecondary),
+        ),
+        centerTitle: true,
+      ),
+      body: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                leading: SvgPicture.asset('assets/icons/security.svg'),
+                title: Text('Security & Password', style: Theme.of(context).textTheme.bodyMedium,),
+              ),
+              ListTile(
+                leading: SvgPicture.asset('assets/icons/star.svg'),
+                title: Text('Rate App', style: Theme.of(context).textTheme.bodyMedium,),
+              ),
+              ListTile(
+                leading: const Icon(Icons.lock_outline),
+                title: Text('Privacy Policy', style: Theme.of(context).textTheme.bodyMedium,),
+              ),
+              const Gap(10),
+              Text(
+                'Assistance',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              ListTile(
+                leading: SvgPicture.asset('assets/icons/file.svg'),
+                title: Text('Terms and Conditions', style: Theme.of(context).textTheme.bodyMedium,),
+              ),
+              ListTile(
+                leading: const Icon(Icons.email_outlined),
+                title: Text('Contact', style: Theme.of(context).textTheme.bodyMedium,),
+              ),
+              const Gap(10),
+              Text(
+                'Action',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              ListTile(
+                leading: SvgPicture.asset('assets/icons/logout.svg'),
+                title: Text('Logout', style: Theme.of(context).textTheme.bodyMedium,),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
