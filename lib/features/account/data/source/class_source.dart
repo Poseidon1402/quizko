20dc309a37d4db18be0986ff7819fd96dbbd27e6
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2024-07-09 19:10:10 +0300

    done:fetch real data

--- lib/features/account/data/source/class_source.dart
+++ lib/features/account/data/source/class_source.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
+import '../../../../core/config/api_config.dart';
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/utils/services/check_status_code.dart';
 import '../models/class_model.dart';
 
 abstract class ClassSource {
@@ -13,10 +19,21 @@ class ClassSourceImpl implements ClassSource {
 
   @override
   Future<List<ClassModel>> fetchAll() async {
-    return const [
-      ClassModel(id: 1, name: 'M1 GID'),
-      ClassModel(id: 2, name: 'M1 GBD'),
-      ClassModel(id: 3, name: 'M1 ASR'),
-    ];
+    try {
+      http.Response response = await client.get(
+        Uri.http(ApiConfig.baseUrl, '/api/levels'),
+      );
+
+      if(isSuccess(response.statusCode)) {
+        final decodedJson = json.decode(utf8.decode(response.bodyBytes));
+        return (decodedJson['data'] as List<dynamic>).map((item) => ClassModel.fromJson(item)).toList();
+      } else {
+        throw ServerException();
+      }
+    } on http.ClientException {
+      throw InternetConnectionException();
+    } on SocketException {
+      throw ServerException();
+    }
   }
 }
